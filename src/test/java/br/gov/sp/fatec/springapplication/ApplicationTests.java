82c3662b2e132d1ce464b37a0e0190d3b5164a2c
Douglas Henrique <dev.dhbarboza@gmail.com>
2022-06-06 14:55:52 +0000

    Adicionado Teste de Cliente e Cliente Compra

--- src/test/java/br/gov/sp/fatec/springapplication/ApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springapplication/ApplicationTests.java
@@ -1,6 +1,9 @@
 package br.gov.sp.fatec.springapplication;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+//import java.beans.Transient;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.sp.fatec.springapplication.entity.Vendedor;
 import br.gov.sp.fatec.springapplication.repository.VendedorRepository;
+import br.gov.sp.fatec.springapplication.entity.Cliente;
+import br.gov.sp.fatec.springapplication.repository.ClienteRepository;
 
 @SpringBootTest
 @Transactional
@@ -18,6 +23,9 @@ class ApplicationTests {
 
 	@Autowired
 	private VendedorRepository vendedorRepo;
+	
+	@Autowired
+	private ClienteRepository clienteRepo;
 
 	@Test
 	void contextLoads() {
@@ -30,6 +38,21 @@ class ApplicationTests {
 		vendedor.setMatricula("05_06@2022");
 		vendedorRepo.save(vendedor);
 		assertNotNull(vendedor.getId());
+	}
+
+	@Test
+	void testInsertCliente() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manarildo");
+		cliente.setMatricula("06_06@2022");
+		clienteRepo.save(cliente);
+		assertNotNull(cliente.getId());
+	}
+
+	@Test
+	void testClienteCompra() {
+		Cliente cliente = new clienteRepo.findById(1L).get();
+		assertNotNull(cliente.getId());
 	}
 
 }
